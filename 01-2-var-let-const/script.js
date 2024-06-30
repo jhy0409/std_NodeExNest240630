ca8defff831e6204ebe3819cc2bdfca507f8575f
jhy0409 <inooph@naver.com>
2024-06-30 23:39:47 +0900

    01-3 scope

--- 01-2-var-let-const/script.js
+++ 01-2-var-let-const/script.js
@@ -20,5 +20,36 @@
 
 
 // 중복 선언 불가, 재할당은 불가
-const greeting = 'hello';
-console.log(greeting);
+// const greeting = 'hello';
+// console.log(greeting);
+
+
+
+// 01-3 scope
+// 유효한 참조범위
+// var => 함수 레벨 스코프
+// function f() {
+//     if (true) {
+//         var a = 'a';
+//         console.log(a);
+//     }
+
+//     console.log(a);
+// }
+
+// // console.log(a); // err
+// f();
+
+
+// let, const => block 레벨 스코프
+
+function f() {
+    if (true) {
+        let a = 'a';
+        console.log(a);
+    }
+
+    // console.log(a); // err
+}
+
+f();
