2bdd4fbde6312072a80930ba315b43e026a8b924
jhy0409 <inooph@naver.com>
2024-06-30 23:31:47 +0900

    01-2 var let const 2

---
+++ 01-2-var-let-const/script.js
@@ -0,0 +1,24 @@
+// var a = 1;
+// console.log(a);
+
+// var greeting = 'hello';
+// console.log(greeting);
+
+// var greeting = 'hi';
+// console.log(greeting);
+
+// var greeting = 'hi 2';
+// console.log(greeting);
+
+
+// 중복 선언 불가, 재할당은 가능
+// let greeting = 'hello';
+// console.log(greeting);
+
+// greeting = 'hi';
+// console.log(greeting);
+
+
+// 중복 선언 불가, 재할당은 불가
+const greeting = 'hello';
+console.log(greeting);
