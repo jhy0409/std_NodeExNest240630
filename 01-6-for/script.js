5dc69281e164f430a9fa974ec2500babac1aa174
jhy0409 <inooph@naver.com>
2024-07-04 05:23:27 +0900

    01-6-for

---
+++ 01-6-for/script.js
@@ -0,0 +1,36 @@
+// for (let i = 0; i<10; i++ ) {
+//     if (i === 3) {
+//         console.log(`\nis ${i}`);
+//         continue;
+//     }
+
+//     if (i === 5) {
+//         console.log(`Stop loop ${i}`);
+//         break;
+//     }
+
+//     console.log(`Num is ${i}`);
+// }
+
+// const user = {
+//     name : 'han',
+//     province : 'kkd',
+//     city : 'snCity'
+// }
+
+// for (let i in user) {
+//     console.log(`${i} is ${user[i]}`);
+// }
+
+// let i = 0;
+// while (i < 10) {
+//     console.log(`${i}`);
+//     i++;
+// }
+
+// let i = 0;
+let i = 100;
+do {
+    console.log(`do - ${i}`);
+    i++;
+} while (i < 10) 
