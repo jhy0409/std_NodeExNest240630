eca041a83d0a852f63389909c90f30eb82bf37f3
jhy0409 <inooph@naver.com>
2024-07-04 05:45:11 +0900

    01-7-window

---
+++ 01-7-window/script.js
@@ -0,0 +1,14 @@
+let va;
+va = window.outerHeight;
+va = window.outerWidth;
+
+va = window.innerHeight;
+va = window.innerWidth;
+
+va = window.scrollY;
+va = window.scrollX;
+
+va = window.location;
+// window.location.href="http://google.com";
+
+console.log(va);
